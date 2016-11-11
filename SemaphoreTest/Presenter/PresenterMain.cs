6b171103ff6731167e68c4168bcd86257609dc2a
Marat Screibicus <maratv.programming@yahoo.com>
2016-11-11 19:52:02 +0200

    Removed unused directives

--- SemaphoreTest/Presenter/PresenterMain.cs
+++ SemaphoreTest/Presenter/PresenterMain.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using SemaphoreTest.View;
-using SemaphoreTest;
 
 namespace SemaphoreTest.Presenter
 {
